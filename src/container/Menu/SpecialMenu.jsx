bfdf6c4e028f21f1dc72428a438a5b1ddcae6173
Rudra Dubey <rudramdev015@gmail.com>
2026-02-16 14:54:14 +0530

    Fixed ESLint unused images error

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import { SubHeading, MenuItem } from '../../components';
-import { data, images } from '../../constants';
+import { data } from '../../constants';   // ✅ removed images
 import './SpecialMenu.css';
 
 // Import local assets
-import naanImg from './Naan.png'; 
-import menuPdf from './Kake Menu.pdf'; 
+import naanImg from './Naan.png';
+import menuPdf from './Kake Menu.pdf';
 
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
@@ -20,21 +20,31 @@ const SpecialMenu = () => (
         <p className="app__specialMenu-menu_heading">Lassi & Shakes</p>
         <div className="app__specialMenu_menu_items">
           {data.lassis.map((lassi, index) => (
-            <MenuItem key={lassi.title + index} title={lassi.title} price={lassi.price} tags={lassi.tags} />
+            <MenuItem
+              key={lassi.title + index}
+              title={lassi.title}
+              price={lassi.price}
+              tags={lassi.tags}
+            />
           ))}
         </div>
       </div>
 
-      {/* REFINED IMAGE SECTION */}
+      {/* IMAGE SECTION */}
       <div className="app__specialMenu-menu_img">
-        <img src={naanImg} alt="menu__img" className="img__glow-animation" />
+        <img src={naanImg} alt="Naan Special" className="img__glow-animation" />
       </div>
 
       <div className="app__specialMenu-menu_mains flex__center">
         <p className="app__specialMenu-menu_heading">Legendary Mains</p>
         <div className="app__specialMenu_menu_items">
           {data.mainCourse.map((dish, index) => (
-            <MenuItem key={dish.title + index} title={dish.title} price={dish.price} tags={dish.tags} />
+            <MenuItem
+              key={dish.title + index}
+              title={dish.title}
+              price={dish.price}
+              tags={dish.tags}
+            />
           ))}
         </div>
       </div>
@@ -42,7 +52,9 @@ const SpecialMenu = () => (
 
     <div style={{ marginTop: 25 }}>
       <a href={menuPdf} target="_blank" rel="noreferrer">
-        <button type="button" className="custom__button">View Full Menu (PDF)</button>
+        <button type="button" className="custom__button">
+          View Full Menu (PDF)
+        </button>
       </a>
     </div>
   </div>
